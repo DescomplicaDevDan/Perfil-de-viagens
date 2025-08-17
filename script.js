716ffd2c9b62e7b8912623382210fed4737bf593
Danilo Texeira <texeirad042@gmail.com>
2025-08-17 13:40:24 -0300

    Adiciona galeria de fotos interativa com modal

---
+++ script.js
@@ -0,0 +1,30 @@
+const galleryImages = document.querySelectorAll('.gallery-image');
+
+const modal = document.getElementById('image-modal');
+const modalImage = document.getElementById('modal-image');
+const modalClose = document.querySelector('.modal-close');
+
+function openModal(imageSrc) {
+    modalImage.src = imageSrc;
+    modal.style.display = 'flex';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+galleryImages.forEach(image => {
+    image.addEventListener('click', () => {
+        openModal(image.src);
+    });
+});
+
+modalClose.addEventListener('click', () => {
+    closeModal();
+});
+
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        closeModal();
+    }
+});
